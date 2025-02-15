46d06bf47bc0e45d108badfebd533a848a369b07
briannjara <briannjaramba138@gmail.com>
2025-02-15 18:40:27 +0300

    third-commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/main/Navbar";
+import Footer from "@/components/main/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +21,9 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
+        <Navbar/>
         {children}
+        <Footer/>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
 import Hero from "@/components/main/Hero";
 import Navbar from "../components/main/Navbar";
+import Skills from "@/components/main/Skills";
+import Encryption from "@/components/main/Encryption";
+import Projects from "@/components/main/Projects";
 
 export default function Home() {
   return (
     <main>
       <div className="flex flex-col gap-20 ">
-        <Navbar/>
         <Hero />
+        <Skills/>
+        <Encryption/>
+        <Projects/>
       </div>
     </main>
   );
