2afb4c1ca76d9f61dd6d79cbf78dea2b00e38584
briannjara <briannjaramba138@gmail.com>
2025-02-15 18:14:04 +0300

    second-commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/main/Hero";
+import Navbar from "../components/main/Navbar";
 
 export default function Home() {
   return (
     <main>
-      <div className="flex flex-col gap-20 h-[850px]">
+      <div className="flex flex-col gap-20 ">
+        <Navbar/>
         <Hero />
       </div>
     </main>
